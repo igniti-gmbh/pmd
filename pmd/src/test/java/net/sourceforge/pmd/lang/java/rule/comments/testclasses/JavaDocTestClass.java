e06979258d6d999d349e05f33a97c54556aaa4f6
Alexander Stillich <a.stillich@igniti.de>
2014-04-10 11:25:14 +0200

    Added negative JavaDoc references test

--- pmd/src/test/java/net/sourceforge/pmd/lang/java/rule/comments/testclasses/JavaDocTestClass.java
+++ pmd/src/test/java/net/sourceforge/pmd/lang/java/rule/comments/testclasses/JavaDocTestClass.java
@@ -1,22 +1,19 @@
 package net.sourceforge.pmd.lang.java.rule.comments.testclasses;
 
+import java.util.List;
+
 /**
  * 
- * This class is used by the JavaDoc comment link tests which need
+ * This class is used by the JavaDoc comment references tests which need
  * the link targets to be compiled and loaded into the VM.
- *
- * <p>A link to the {@link net.sourceforge.pmd.lang.java.rule.comments.testclasses.JavaDocTestClass test class}.</p>
- * <p>A link to a {@link JavaDocTestClass#method1() method}.</p>
- * <p>A link to the {@link JavaDocTestClass#overload1 overload method}.</p>
- * <p>A link to the first {@link JavaDocTestClass#overload1(int param1) overload method}.</p>
- * <p>A link to the second {@link JavaDocTestClass#overload1(int, int) overload method}.</p>
- * <p>A link to the third {@link JavaDocTestClass#overload1(String, float) overload method}.</p>
- * <p>A link to the fourth {@link JavaDocTestClass#overload2(E) overload method}.</p>
- * <p>A link to the fifth {@link JavaDocTestClass#overload2(E, F) overload method}.</p>
  * 
  */
 public class JavaDocTestClass {
 
+	public int field1;
+
+	public static final int field2 = 0;
+	
 	public void method1() {}
 	
 	public void overload1(int param1) {}
@@ -25,7 +22,7 @@ public class JavaDocTestClass {
 
 	public void overload1(String param1, float param2) {}
 
-	public <E> void overload2(E param1) {}
-	
+	public <E extends List<?>> void overload2(E param1) {}
+
 	public <E, F> void overload2(E param1, F param2) {}
 }
